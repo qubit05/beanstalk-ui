8f85baa051af22f679d13c1c0faed4af0cbce0fb
Luke Richards <luke.richards@emailcenteruk.com>
2014-07-03 19:22:11 +0100

    Changed the service so it returns an indexed array rather than associative... much more useful.

--- src/Bui/BuiService.php
+++ src/Bui/BuiService.php
@@ -8,19 +8,30 @@ class BuiService
      */
     protected $server;
 
+    /**
+     * Constructor
+     *
+     * @param \Pheanstalk\Pheanstalk $server
+     * @return void
+     */
     public function __construct(\Pheanstalk\Pheanstalk $server)
     {
         $this->server = $server;
     }
 
+    /**
+     * Get all statistics for all tubes.
+     *
+     * @return array
+     */
     public function getAllStats()
     {
         $allStats = array();
-        $tubes = $this->server->listTubes();
+        $tubes    = $this->server->listTubes();
         foreach ($tubes as $tubeName) {
             $tubeStats = $this->server->statsTube($tubeName);
             /* @var $tubeStats \Pheanstalk\Response\ArrayResponse */
-            $allStats[$tubeStats['name']] = $tubeStats;
+            $allStats[] = $tubeStats;
         }
         return $allStats;
     }
