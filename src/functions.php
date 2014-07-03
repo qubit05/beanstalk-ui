a1faf776c3178ccb9a86bf42f2907512788a97d7
Luke Richards <luke.richards@emailcenteruk.com>
2014-07-03 19:21:30 +0100

    Special function to convert the array keys so they can be used on JS.

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,24 @@
 <?php
+
+/**
+ *
+ * @param array $data
+ * @return array
+ */
+function make_json_safe_keys(array $data)
+{
+    if (!isset($data[0])) {
+        // assoc
+        return make_json_safe_keys(array($data))[0];
+    }
+
+    $cleaned = array();
+    foreach ($data as $position => $row) {
+        $cleaned[$position] = array();
+        foreach ($row as $column => $value) {
+            $column = str_replace('-', '_', $column);
+            $cleaned[$position][$column] = $value;
+        }
+    }
+    return $cleaned;
+}
