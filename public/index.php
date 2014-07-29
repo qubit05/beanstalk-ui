74008e6234331b2f287b7f582f4126f2a70d88c0
Luke Richards <luke.richards@emailcenteruk.com>
2014-07-29 09:52:14 +0100

    Changed the naming for lots of routes. Should now be more semantically consistent with beanstalk.

--- public/index.php
+++ public/index.php
@@ -19,6 +19,24 @@ $app->configureMode('production', function() use ($app) {
 });
 
 // dependency container
+$app->container->httpClient = function() {
+    return new \Guzzle\Http\Client();
+};
+$app->container->singleton('githubApi', function() use ($app) {
+    $client = $app->container->httpClient;
+    $client->setBaseUrl($app->config('version.base_url'));
+
+    $cache = new \Guzzle\Plugin\Cache\CachePlugin(array(
+        'storage' => new \Guzzle\Plugin\Cache\DefaultCacheStorage(
+            new \Guzzle\Cache\DoctrineCacheAdapter(
+                new Doctrine\Common\Cache\FilesystemCache($app->config('storage.cache.http'))
+            )
+        )
+    ));
+
+    $client->addSubscriber($cache);
+    return $client;
+});
 $app->container->singleton('beanstalk', function () use ($app) {
     $host = $app->config('beanstalk.host');
     $port = $app->config('beanstalk.port');
@@ -29,7 +47,33 @@ $app->container->singleton('buiService', function () use ($app) {
 });
 
 // routing
-$app->get('/summary', function() use ($app) {
+$app->get('/server', function() use ($app) {
+    $retries = 3;
+    while ($retries > 0) {
+        try {
+            $server = $app->container->beanstalk;
+            /* @var $server \Pheanstalk\Pheanstalk */
+        } catch (\Pheanstalk\Exception\ConnectionException $e) {
+            if ($retries == 0) {
+                throw $e;
+            }
+        }
+        $retries--;
+    }
+
+    $app->response->header('Content-Type', 'application/json');
+    $app->response->setBody(json_encode(make_json_safe_keys((array)$server->stats())));
+});
+$app->get('/server/versions', function() use ($app) {
+    $client = $app->container->httpClient;
+    /* @var $client \Guzzle\Http\Client */
+    $client->setBaseUrl($app->config('versions.base_url'));
+    $apiResponse = $client->get('repos/kr/beanstalkd/tags')->send();
+
+    $app->response->header('Content-Type', 'application/json');
+    $app->response()->setBody(json_encode($apiResponse->json()));
+});
+$app->get('/tube', function() use ($app) {
     $retries = 3;
     while ($retries > 0) {
         try {
@@ -43,12 +87,10 @@ $app->get('/summary', function() use ($app) {
         $retries--;
     }
 
-    $response = $app->response();
-    $response->header('Content-Type', 'application/json');
-    $response->status(200);
-    $response->setBody(json_encode(make_json_safe_keys($service->getAllStats())));
+    $app->response->header('Content-Type', 'application/json');
+    $app->response->setBody(json_encode(make_json_safe_keys($service->getAllStats())));
 });
-$app->get('/tube-stats/:name', function($name) use ($app) {
+$app->get('/tube/:name', function($name) use ($app) {
     $retries = 3;
     while ($retries > 0) {
         try {
@@ -62,12 +104,24 @@ $app->get('/tube-stats/:name', function($name) use ($app) {
         $retries--;
     }
 
-    $response = $app->response();
-    $response->header('Content-Type', 'application/json');
-    $response->status(200);
-    $response->setBody(json_encode(make_json_safe_keys($service->getTubeStats($name))));
+    $app->response->header('Content-Type', 'application/json');
+    $app->response->setBody(json_encode(make_json_safe_keys($service->getTubeStats($name))));
 });
+$app->put('/tube/:name(/:quantity)', function($name, $quantity = 1) {
+    // kick a number of jobs on tube
+})->conditions(array('quantity' => '\d+'));
+$app->get('/job/:id', function() use ($app) {
+    // peek at a job
+})->conditions(array('id' => '\d+'));
+$app->post('/job', function() use ($app) {
+    // add a new job
+});
+$app->delete('/job/:id', function() use ($app) {
+    // delete a job
+})->conditions(array('id' => '\d+'));
 $app->get('/', function() use ($app) {
     $app->render('layout.phtml');
 });
+
+// finally run!
 $app->run();
